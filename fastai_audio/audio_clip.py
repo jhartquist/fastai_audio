6baa09d17db1f45814957cfa6be21c963ab172de
John Hartquist <sevenfx7@gmail.com>
2018-11-27 11:50:30 -0800

    Update AudioClip show() and show_batch()

--- fastai_audio/audio_clip.py
+++ fastai_audio/audio_clip.py
@@ -1,5 +1,6 @@
 from fastai import *
 from scipy.io import wavfile
+from IPython.display import display, Audio
 
 __all__ = ['AudioClip', 'open_audio']
 
@@ -20,6 +21,24 @@ class AudioClip(ItemBase):
     @property
     def duration(self):
         return self.num_samples / self.sample_rate
+
+    def show(self, ax=None, figsize=(5, 1), player=True, title=None, **kwargs):
+        if ax is None:
+            _, ax = plt.subplots(figsize=figsize)
+        if title:
+            ax.set_title(title)
+        timesteps = np.arange(len(self.data)) / self.sample_rate
+        ax.plot(timesteps, self.data)
+        ax.set_xlabel('Time (s)')
+        if player:
+            # unable to display an IPython 'Audio' player in plt axes
+            display(Audio(self.data, rate=self.sample_rate))
+        plt.show()
+
+    def show_batch(self, idxs, rows, ds, **kwargs):
+        for i in idxs[:rows]:
+            x,y = ds[i]
+            x.show(title=y, **kwargs)
 
 
 def open_audio(fn):
